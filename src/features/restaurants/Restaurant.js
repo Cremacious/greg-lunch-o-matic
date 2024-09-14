28a023f5c101ebcebb45ccf0649540c644a54634
Cremacious <108367916+Cremacious@users.noreply.github.com>
2024-09-13 23:00:11 -0400

    Make changes to Comments display and added selectComments

--- src/features/restaurants/Restaurant.js
+++ src/features/restaurants/Restaurant.js
@@ -1,13 +1,22 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import { selectRestaurants } from "./restaurantsSlice";
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { selectRestaurants } from './restaurantsSlice';
+
 
 export function Restaurant() {
-    const restaurants = useSelector(selectRestaurants);
-    
-    return (
-        <div>
-        <h1>Restaurant</h1>
-        </div>
-    );
+  const restaurants = useSelector(selectRestaurants);
+
+  return (
+    <div>
+      {restaurants.map((restaurant) => {
+        return (
+          <li key={restaurant.id}>
+            <h2>{restaurant.name}</h2>
+            <p>{restaurant.location}</p>
+            {/* <Comments restaurantId={restaurant.restaurantId} /> */}
+          </li>
+        );
+      })}
+    </div>
+  );
 }

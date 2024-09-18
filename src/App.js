d28f2ea551b44be2891937f1e8092e738cb4bb1e
Cremacious <108367916+Cremacious@users.noreply.github.com>
2024-09-18 19:21:09 -0400

    Added Bootstrap design.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,22 @@ import React from 'react';
 import { Restaurants } from './features/restaurants/Restaurants';
 import './App.css';
 
-
 function App() {
   return (
     <div className="App">
       <Restaurants />
+      <div class="container">
+        <div class="card">
+          <div class="card-body">
+            <button class="btn btn-primary" type="button">
+              Add Restaurant
+            </button>
+            <button class="btn btn-primary" type="button">
+              Manage Restaurants
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }

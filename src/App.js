8a09bce70ea7bc5ae19862e6ae35ce3c5d4bd8b2
Cremacious <108367916+Cremacious@users.noreply.github.com>
2024-09-19 19:54:43 -0400

    Setup for testing restaurant form added.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { Restaurants } from './features/restaurants/Restaurants';
 import { RestaurantForm } from './features/restaurants/RestaurantForm';
+import {Restaurant} from './features/restaurants/Restaurant';
 import './App.css';
 
 function App() {
@@ -12,6 +13,8 @@ function App() {
           <Route path="/" element={<RestaurantForm />} />
           <Route path="/restaurants" element={<Restaurants />} />
         </Routes>
+      <Restaurant />
+      <Restaurants />
         <div className="container">
           <div className="card">
             <div className="card-body">

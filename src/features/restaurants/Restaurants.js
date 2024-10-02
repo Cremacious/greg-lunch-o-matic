173e6386c046ba1553dff8062588ab69777f5d57
Cremacious <108367916+Cremacious@users.noreply.github.com>
2024-10-02 16:10:28 -0400

    Search box now filters all restaurants.

--- src/features/restaurants/Restaurants.js
+++ src/features/restaurants/Restaurants.js
@@ -3,7 +3,7 @@ import { useSelector } from 'react-redux';
 import { useState } from 'react';
 import { selectRestaurants } from './restaurantsSlice';
 import { Link } from 'react-router-dom';
-import { SearchBar } from '../../components/Search/SearchBar';
+// import { SearchBar } from '../../components/Search/SearchBar';
 
 export function Restaurants() {
   const restaurants = useSelector(selectRestaurants);
@@ -17,13 +17,6 @@ export function Restaurants() {
     <section style={{ marginTop: '22px' }}>
       <h1>Test</h1>
       <input type="search" onChange={onSearchChange} />
-      {restaurants
-        .filter((restaurant) =>
-          restaurant.name.toLowerCase().includes(searchTerm)
-        )
-        .map((filteredRestaurant) => (
-          <h1 key={filteredRestaurant.id}>{filteredRestaurant.name}</h1>
-        ))}
       <div className="container">
         <div className="card">
           <div
@@ -48,96 +41,104 @@ export function Restaurants() {
                     Restaurants
                   </h1>
                 </div>
-                {restaurants.map((restaurant) => (
-                  <div
-                    key={restaurant.id}
-                    style={{ marginTop: '10px', marginBottom: '10px' }}
-                  >
+                {restaurants
+                  .filter((restaurant) =>
+                    restaurant.name.toLowerCase().includes(searchTerm)
+                  )
+                  .map((filteredRestaurant) => (
                     <div
-                      className="card"
-                      style={{ background: 'var(--bs-form-valid-color)' }}
+                      key={filteredRestaurant.id}
+                      style={{ marginTop: '10px', marginBottom: '10px' }}
                     >
-                      <div className="card-body">
-                        <div className="card" style={{ marginBottom: '10px' }}>
+                      <div
+                        className="card"
+                        style={{ background: 'var(--bs-form-valid-color)' }}
+                      >
+                        <div className="card-body">
                           <div
-                            className="card-body"
-                            style={{
-                              marginTop: '-2px',
-                              paddingTop: '10px',
-                              paddingBottom: '0px',
-                            }}
+                            className="card"
+                            style={{ marginBottom: '10px' }}
                           >
-                            <div>
-                              <h4
-                                className="text-center"
-                                style={{ fontFamily: "'Turret Road', serif" }}
-                              >
-                                {restaurant.name}
-                              </h4>
-                            </div>
-                          </div>
-                        </div>
-                        <div className="row">
-                          <div className="col">
-                            <div className="text-center">
-                              <div className="card">
-                                <div className="card-body">
-                                  <h5
-                                    style={{
-                                      fontFamily: "'Turret Road', serif",
-                                    }}
-                                  >
-                                    Location
-                                  </h5>
-                                  <p
-                                    className="text-center"
-                                    style={{
-                                      fontFamily: "'Turret Road', serif",
-                                    }}
-                                  >
-                                    {restaurant.location}
-                                  </p>
-                                </div>
+                            <div
+                              className="card-body"
+                              style={{
+                                marginTop: '-2px',
+                                paddingTop: '10px',
+                                paddingBottom: '0px',
+                              }}
+                            >
+                              <div>
+                                <h4
+                                  className="text-center"
+                                  style={{ fontFamily: "'Turret Road', serif" }}
+                                >
+                                  {filteredRestaurant.name}
+                                </h4>
                               </div>
                             </div>
                           </div>
-                          <div className="col">
-                            <div className="card">
-                              <div className="card-body">
-                                <div className="text-center">
-                                  <Link
-                                    to={`/restaurant/${restaurant.name}`}
-                                    className="btn btn-success"
-                                  >
-                                    Check It Out!
-                                  </Link>
+                          <div className="row">
+                            <div className="col">
+                              <div className="text-center">
+                                <div className="card">
+                                  <div className="card-body">
+                                    <h5
+                                      style={{
+                                        fontFamily: "'Turret Road', serif",
+                                      }}
+                                    >
+                                      Location
+                                    </h5>
+                                    <p
+                                      className="text-center"
+                                      style={{
+                                        fontFamily: "'Turret Road', serif",
+                                      }}
+                                    >
+                                      {filteredRestaurant.location}
+                                    </p>
+                                  </div>
                                 </div>
                               </div>
                             </div>
-                          </div>
-                          <div className="col">
-                            <div className="text-center">
+                            <div className="col">
                               <div className="card">
                                 <div className="card-body">
-                                  <h5
-                                    style={{
-                                      fontFamily: "'Turret Road', serif",
-                                    }}
-                                  >
-                                    Greg Says:
-                                  </h5>
-                                  <p
-                                    className="text-center"
-                                    style={{
-                                      fontFamily: "'Turret Road', serif",
-                                    }}
-                                  >
-                                    {
-                                      restaurant.comments[
-                                        restaurant.comments.length - 1
-                                      ].comment
-                                    }
-                                  </p>
+                                  <div className="text-center">
+                                    <Link
+                                      to={`/restaurant/${filteredRestaurant.name}`}
+                                      className="btn btn-success"
+                                    >
+                                      Check It Out!
+                                    </Link>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                            <div className="col">
+                              <div className="text-center">
+                                <div className="card">
+                                  <div className="card-body">
+                                    <h5
+                                      style={{
+                                        fontFamily: "'Turret Road', serif",
+                                      }}
+                                    >
+                                      Greg Says:
+                                    </h5>
+                                    <p
+                                      className="text-center"
+                                      style={{
+                                        fontFamily: "'Turret Road', serif",
+                                      }}
+                                    >
+                                      {
+                                        filteredRestaurant.comments[
+                                          filteredRestaurant.comments.length - 1
+                                        ].comment
+                                      }
+                                    </p>
+                                  </div>
                                 </div>
                               </div>
                             </div>
@@ -145,8 +146,7 @@ export function Restaurants() {
                         </div>
                       </div>
                     </div>
-                  </div>
-                ))}
+                  ))}
               </div>
             </div>
           </div>
